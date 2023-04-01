2d0a780ec5a345f7624c550ddd2849839ed81b04
DKeNTA <kentacky.donald@gmail.com>
2023-04-01 10:43:29 +0900

    update: implement reordering

--- todo/urls.py
+++ todo/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:folder_id>/tasks/<int:task_id>', views.EditTaskView.as_view(), name='tasks.edit'),
     path('<int:folder_id>/delete', views.delete_folder, name='folders.delete'),
     path('<int:folder_id>/tasks/<int:task_id>/delete', views.delete_task, name='tasks.delete'),
+    path('<int:folder_id>/update_order', views.UpdateOrderView.as_view(), name='update_order')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,19 @@
+import json
+
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView 
 from django.core.exceptions import PermissionDenied
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.db.models import Max
 from .models import Folder, Task
 from .forms import RegisterForm, LoginForm, FolderForm, TaskForm
 
@@ -33,7 +40,7 @@ class IndexView(LoginRequiredMixin, ListView):
     context_object_name = 'folders'
 
     def get_queryset(self):
-        return Folder.objects.filter(user=self.request.user, created_at__lte=timezone.now()).order_by('created_at')
+        return Folder.objects.filter(user=self.request.user, created_at__lte=timezone.now()).order_by('order')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -51,9 +58,38 @@ class TasksIndexView(LoginRequiredMixin, ListView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['folders'] = Folder.objects.filter(user=self.request.user, created_at__lte=timezone.now()).order_by('created_at')
+        context['folders'] = Folder.objects.filter(user=self.request.user, created_at__lte=timezone.now()).order_by('order')
         context['current_folder_id'] = self.kwargs['folder_id']
         return context
+
+class UpdateOrderView(LoginRequiredMixin, View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        type = data.get('type')
+        order = data.get('order')
+
+        if type == 'folder':
+            folders = Folder.objects.filter(user=request.user, id__in=order)
+            folder_order = {folder.id: folder for folder in folders}
+
+            for index, folder_id in enumerate(order):
+                folder_order[int(folder_id)].order = index
+                folder_order[int(folder_id)].save()
+
+        elif type == 'task':
+            folder_id = data.get('parent_id')
+            tasks = Task.objects.filter(folder_id=folder_id, id__in=order)
+            task_order = {task.id: task for task in tasks}
+
+            for index, task_id in enumerate(order):
+                task_order[int(task_id)].order = index
+                task_order[int(task_id)].save()
+
+        return JsonResponse({'status': 'success'})
     
 class CreateFolderView(LoginRequiredMixin, CreateView):
     model = Folder
@@ -64,6 +100,8 @@ class CreateFolderView(LoginRequiredMixin, CreateView):
         folder = form.save(commit=False)
         folder.user = self.request.user
         folder.created_at = timezone.now()
+        max_order = Folder.objects.filter(user=self.request.user).aggregate(Max('order'))['order__max'] or 0
+        folder.order = max_order + 1
         folder.save()
         return redirect('tasks.index', folder_id=folder.id)
     
@@ -76,6 +114,8 @@ class CreateTaskView(LoginRequiredMixin, CreateView):
         task = form.save(commit=False)
         task.created_at = timezone.now()
         task.folder = get_object_or_404(Folder, id=self.kwargs['folder_id'], user=self.request.user)
+        max_order = Task.objects.filter(folder_id=task.folder.id).aggregate(Max('order'))['order__max'] or 0
+        task.order = max_order + 1
         task.save()
         return redirect('tasks.index', folder_id=task.folder.id)
     
